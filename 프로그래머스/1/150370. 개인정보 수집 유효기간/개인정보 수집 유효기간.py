07a081d920a9b813e922c857829ba6ead4125a4b
Seongbin <122718910+csb1320@users.noreply.github.com>
2024-12-01 10:04:25 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -0,0 +1,17 @@
+def solution(today, terms, privacies):
+    today = list(map(int, today.split('.'))) # [0]연, [1]월, [2]일
+    terms = {i[0]: int(i[2:])for i in terms}
+    
+    today = today[0] * 28 * 12 + today[1] * 28 + today[2] # 일 단위로 변환
+    
+    answer = []
+    for i, pri in enumerate(privacies):
+        day, kind = pri.split()
+        month = terms[kind]
+        day = list(map(int, day.split('.')))
+        day = day[0] * 28 * 12 + day[1] * 28 + day[2]-1 + month * 28 # 일 단위로 변환
+        
+        if day < today:
+            answer.append(i+1)
+                    
+    return answer
